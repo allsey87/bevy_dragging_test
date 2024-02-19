964d31291e58cd6e343722de8829e872c8e55b43
Michael Allwright <allsey87@gmail.com>
2024-02-19 12:28:20 +0100

    Basic version working
    Only XZ dragging is enabled, the rest is zeroed out since pulling the object off the ground or pushing it into the ground changes to the normal force and hence friction force between the object and the ground, leading to counter-intuitive results.
    
    TODOs:
    - Improve mouse tracking with lower camera altitudes
    - Make pulling impulse porportional to weight?
    - Use PID control when applying impulse

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,25 @@ use bevy_rapier3d::prelude::*;
 use bevy_mod_picking::prelude::*;
 use camera::PanOrbitCamera;
 
-fn _render_origin(mut gizmos: Gizmos) {
+fn render_origin(mut gizmos: Gizmos) {
     gizmos.line(Vec3::ZERO, Vec3::X, Color::RED);
     gizmos.line(Vec3::ZERO, Vec3::Y, Color::GREEN);
     gizmos.line(Vec3::ZERO, Vec3::Z, Color::BLUE);
+}
+
+#[derive(Component)]
+struct DragTarget {
+    // the camera on which this drag is occuring
+    camera: Entity,
+
+    // allows calculating the drag target from the mouse
+    origin: Vec3,
+
+    // the offset from the center of mass where the drag started
+    offset: Vec3,
+
+    // distance of the drag (as last reported by events<pointer<drag>>)
+    distance: Vec2,
 }
 
 pub fn main() {
@@ -31,8 +46,9 @@ pub fn main() {
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         //.add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, setup)
-        //.add_systems(Update, render_origin)
+        .add_systems(Update, render_origin)
         .add_systems(Update, (camera::update_camera_system, camera::accumulate_mouse_events_system))
+        .add_systems(Update, drag_system)
         .run();
 }
 
@@ -42,7 +58,7 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // pan-orbit camera with relative directional light
-    let translation = Vec3::new(0.5, 0.5, 0.5);
+    let translation = Vec3::new(0.0, 0.5, 0.0);
     let focus = Vec3::new(0.0, 0.1, 0.0);
     let transform = Transform::from_translation(translation)
         .looking_at(focus, Vec3::Y);
@@ -78,19 +94,13 @@ fn setup(
         .with_children(|commands| {
             commands.spawn(PbrBundle {
                 mesh: meshes.add(shape::Plane::from_size(1.0).into()),
-                material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
+                material: materials.add(Color::rgba(0.9, 0.9, 0.9, 0.5).into()),
                 transform: Transform::from_xyz(0.0, 0.1, 0.0),
                 ..default()
             });
         });
 
     // box
-    #[derive(Component)]
-    struct DragTarget {
-        camera: Entity,
-        offset: Option<Vec3>,
-    }
-
     commands
         .spawn((Collider::cuboid(0.05, 0.05, 0.05), RigidBody::Dynamic))
         .insert(ColliderMassProperties::Mass(1.0))
@@ -98,74 +108,72 @@ fn setup(
         .with_children(|commands| {
             commands.spawn(PbrBundle {
                 mesh: meshes.add(shape::Box::new(0.1, 0.1, 0.1).into()),
-                material: materials.add(Color::rgb(0.9, 0.1, 0.1).into()),
+                material: materials.add(Color::RED.into()),
                 ..default()
             });
         })
         .insert(ExternalImpulse::default())
         .insert(PickableBundle::default())
-        
         // DRAG START
         .insert(On::<Pointer<DragStart>>::run(|
             listener: Listener<Pointer<DragStart>>,
             target: Query<&GlobalTransform, With<ExternalImpulse>>,
             mut commands: Commands| {
-            
-            let target_transform = target.get_single().unwrap();
-            let mut entity_commands = commands.entity(listener.target());
-            
-            entity_commands.insert(DragTarget {
-                camera: listener.hit.camera,
-                offset: listener.hit.position.map(|position| target_transform.affine()
-                    .inverse()
-                    .transform_point3(position)
-                ),
-            });
-        }))
-
-        // DRAG
-        .insert(On::<Pointer<Drag>>::run(|
-            _: Listener<Pointer<Drag>>,
-            mut target: Query<(&DragTarget, &GlobalTransform, &mut ExternalImpulse)>,
-            cameras: Query<(&Camera, &GlobalTransform)>,
-            windows: Query<&Window>,
-            mut gizmos: Gizmos,
-            | {
-            if let Ok((drag_target, transform, mut external_impulse)) = target.get_single_mut() {
-                let (camera, camera_transform) = cameras.get(drag_target.camera).unwrap();
-                let window = windows.get_single().unwrap();
-
-                let ray = window.cursor_position()
-                    .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor));
-                
-                if let Some(ray) = ray {
-                    let intersection = ray.intersect_plane(Vec3::ZERO, Vec3::Y);
-                    if let Some(distance) = intersection {
-                        
-                        // apply force at an offset if the backend gave us this information
-                        let application_point = match drag_target.offset {
-                            Some(offset) => transform.transform_point(offset),
-                            None => transform.translation(),
-                        };
-
-                        gizmos.sphere(application_point, Quat::default(), 0.005, Color::BLUE);
-                        
-                        // apply a force that is proportional to the distance between the object and the cursor
-                        let impulse = (ray.get_point(distance) - application_point) * 1.0;
-                        external_impulse.impulse = impulse;
-                        if let Some(offset) = drag_target.offset {
-                            external_impulse.torque_impulse = offset.cross(impulse);
-                        }
-                    }        
-                }
+            if listener.button == PointerButton::Primary {
+                let target_transform = target.get_single().unwrap();
+                let position = listener.hit.position
+                    .expect("backend does not support `position`");
+                commands.entity(listener.target()).insert(DragTarget {
+                    camera: listener.hit.camera,
+                    origin: position,
+                    offset: target_transform.affine().inverse().transform_point3(position),
+                    distance: Default::default()
+                });
             }
         }))
-
         // DRAG END
         .insert(On::<Pointer<DragEnd>>::target_remove::<DragTarget>());
-        
 }
 
+fn drag_system(
+    mut drag_events: EventReader<Pointer<Drag>>,
+    mut target: Query<(&mut DragTarget, &GlobalTransform, &mut ExternalImpulse)>,
+    camera_transforms: Query<&GlobalTransform, With<Camera>>,
+) {
+    if let Ok((mut target, target_transform, mut target_force)) = target.get_single_mut() {
+        /* update the cached target distance */
+        if let Some(last_drag_event) = drag_events.iter().last() {
+            target.distance = last_drag_event.distance;
+        }
+
+        /* convert drag target distance  */
+        let camera_transform = camera_transforms
+            .get(target.camera)
+            .unwrap();
+        let mut drag_target_offset = camera_transform.translation() +
+            target.distance.x * camera_transform.right() -
+            target.distance.y * camera_transform.up();
+        drag_target_offset.y = 0.0;
+
+        // TODO: improve zoom factor for lower camera altitudes
+        let zoom_factor = (camera_transform.translation() - target.origin).length() * 0.0011;
+        let drag_target = target.origin + (drag_target_offset * zoom_factor);
+        let drag_point = target_transform.transform_point(target.offset);
+
+        // TODO: make gain a factor of object weight
+        const GAIN: f32 = 1.5;
+        // TODO: use PID control?
+        let drag_impulse = (drag_target - drag_point)
+            .clamp(Vec3::NEG_ONE, Vec3::ONE) * GAIN;
+        target_force.impulse = drag_impulse;
+
+        let mut drag_com_offset = drag_point - target_transform.translation();
+        drag_com_offset.y = 0.0;
+
+        let orthogonal_vector = (drag_com_offset) - (drag_com_offset).project_onto(drag_impulse);
+        target_force.torque_impulse = orthogonal_vector.cross(drag_impulse);
+    }
+}
 
 
 
